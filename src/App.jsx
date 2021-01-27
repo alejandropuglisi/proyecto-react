a13e630b1d055838e91980131eb9c70cf6d127f2
Alejandro Puglisi <alejandro.puglisi@gmail.com>
2021-01-27 17:34:39 -0300

    master

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/navbar';
+
+const App = () => {
+  return (
+    <>
+      <NavbarComponent/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import './navbar.css';
+
+const NavbarComponent = () => {
+  return (
+    <>
+        <Navbar variant="dark">
+            <img className="logo" src="/images/logo.png" alt="Logo"/>
+            <Nav className="mr-auto dropdown">
+                <button type="button" class="btn hover">Inicio</button>
+                <button type="button" class="btn dropdown-toggle hover" data-toggle="dropdown" data-hover="dropdown">Productos</button>
+                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                </NavDropdown>
+                <button type="button" class="btn dropdown-toggle hover active" data-toggle="dropdown">Salud</button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                    </div>
+                    <button type="button" class="btn hover">Adiestramiento</button>
+            </Nav>
+            <img className="pata" src="/images/pata.png" alt="pata"/>
+            <Form inline>
+                <FormControl type="text" placeholder="Búsqueda" className="mr-sm-2" />
+                <Button id="search-button">Buscar</Button>
+            </Form>
+        </Navbar>
+    </>
+  );
+};
+
+export default NavbarComponent;
